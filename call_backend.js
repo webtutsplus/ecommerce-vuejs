88ba971aef5de4ded8820b355b573eed78c84bcd
nmadhab <nilmadhab1994@booking.com>
2020-12-16 21:59:15 +0000

    sample api call

--- call_backend.js
+++ call_backend.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $.get( "http://localhost:8080/api/user/", function( data ) {
+    $.get( "http://167.99.44.132:8080/api/user/", function( data ) {
         $( "#result" ).html( data[0].firstName);
         console.log(data[0].firstName);
     });
