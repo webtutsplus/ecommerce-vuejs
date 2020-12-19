05972d09b1923ec6ad544bb316acf6fd0e7174a5
Jean-Jacques <jeanjacques.strydom34@gmail.com>
2020-12-19 18:10:58 +0200

    Added ability to add products

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,48 @@
 
 // Add the cards to the card container
 $("#card-container").ready(async function() {
-    var cards = await talkToServer();
+    var cards = await getAllProducts();
 
     for (const index in cards) {
         var card = cards[index];
         $("#card-container").append(card);
+    }
+});
+
+// Alert the user when they "bought" a product
+$("#card-container").on("click", "#buy-btn", function() {
+    alert("Thank you for supporting https://www.webtutsplus.com");
+    // window.location.href = "index.html";
+});
+
+// Handle page changes to add-product.html
+$("#add-product").on("click", function() {
+    window.location.href = "add-product.html";
+    console.log("Page should have changed");
+});
+
+// Handle form submit
+$("#product-form").submit(async function(form) {
+    form.preventDefault();
+
+    const name = $("#productNameInput").val();
+    const url = $("#productImageURLInput").val();
+    const price = $("#productPriceInput").val();
+    const description = $("#productDescriptionInput").val();
+
+    const product = {
+        "name": name,
+        "imageURL": url,
+        "price": price,
+        "description": description
+    };
+
+    var result = await createNewProduct(product);
+
+    if (result) {
+        alert("Product has been added!");
+        window.location.href = "index.html";
+    } else {
+        alert("Failed to add product, sorry");
     }
 });
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,54 +1,68 @@
 // Useful functions to have
 
 // Function to talk to the server and return a bunch of products
-async function talkToServer() {
+async function getAllProducts() {
     var products = [];
     var cards = [];
+    const errorMessage = "<p>We don't currently have any items in stock, but feel free to add your own product/s </p>";
     
     await fetch('http://remotedevs.org:8080/api/product/', {
         method: 'GET',
         mode: 'cors'
     })
     .then(response => response.json())
-    .then(data => products = data);
+    .then(data => products = data)
+    .catch(_ => cards.push(errorMessage));
 
-    /*
-    {
-        "id": 1,
-        "name": "Adidas Final UCL Ball",
-        "imageURL": "https://images.unsplash.com/photo-1589487391730-58f20eb2c308?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vdGJhbGx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        "price": 3500,
-        "description": "This is a football"
-    }
-    */
-    for (const index in products) {
-        var product = products[index];
-    
-        var productID = product.id;
-        var productName = product.name;
-        var productImageURL = product.imageURL;
-        var productPrice = product.price;
-        var productDescription = product.description;
+    if (products.length > 0 && cards.length < 1) {
+        for (const index in products) {
+            var product = products[index];
+        
+            var productID = product.id;
+            var productName = product.name;
+            var productImageURL = product.imageURL;
+            var productPrice = product.price;
+            var productDescription = product.description;
 
-        var card = `
-            <div class="col" id=${productID}>
-                <div class="card bg-dark" style="width: 18rem;">
-                    <img class="card-img-top" src=${productImageURL} alt="Product Image">
-                    <div class="card-body text-white">
-                        <h5 class="card-title">${productName}</h5>
-                        <p class="card-text">${productDescription}</p>
-                        <br />
-                        <p><strong>Price: $</strong> ${productPrice}</p>
-                    </div>
-                    <div class="card-footer bg-transparent text-center">
-                        <button type="button" class="btn btn-warning btn-lg btn-block">Buy Product</button>
+            var card = `
+                <div class="col-sm" id=${productID}>
+                    <div class="card bg-dark" style="width: 18rem;">
+                        <img class="card-img-top" src=${productImageURL} alt="Product Image">
+                        <div class="card-body text-white">
+                            <h5 class="card-title">${productName}</h5>
+                            <p class="card-text">${productDescription}</p>
+                            <br />
+                            <p><strong>Price: $</strong> ${productPrice}</p>
+                        </div>
+                        <div class="card-footer bg-transparent text-center">
+                            <button type="button" class="btn btn-outline-warning btn-lg btn-block" id="buy-btn">Buy Product</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `;
+            `;
 
-        cards.push(card);
+            cards.push(card);
+        }
+    } else if (products.length < 1 && cards.length < 1) {
+        cards.push(errorMessage);
     }
 
     return cards;
+}
+
+// Create a new product
+async function createNewProduct(product) {
+    var result = false;
+
+    await fetch('http://remotedevs.org:8080/api/product/add/', {
+        method: 'POST',
+        body: JSON.stringify(product),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(_ => result = true)
+    .catch(_ => result = false);
+
+    return result;
 }
