231318c60e1b251f96073052541b0f8a7afdf6ac
ChitreshApte <60211927+ChitreshApte@users.noreply.github.com>
2020-12-26 10:13:38 +0530

    Footer.vue, Navbar.vue added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <div class="nav">
-      <router-link :to="{name : 'Home'}">
-        <img src="./assets/logo.png" alt="Webtutsplus logo" class="logo">
-      </router-link>
-      <ul class="main-nav">
-          <li><router-link :to="{name : 'Home'}" >HOME</router-link></li>
-          <li><router-link :to="{name : 'Product'}">PRODUCT</router-link></li>
-          <li><router-link :to="{name : 'Category'}">CATEGORY</router-link></li>
-      </ul>
-  </div>
+  <Navbar/>
   <router-view :baseURL="baseURL" :products="products" :categories="categories"></router-view>
-  <div v-show="this.$route.name != 'Home'">
-    <Footer></Footer>
-  </div>
+  <Footer v-show="this.$route.name != 'Home'" />
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue"
 import Footer from "./components/Footer.vue"
 export default {
   data() {
@@ -25,7 +15,7 @@ export default {
       categories : null,
     }
   },
-  components : {Footer},
+  components : {Footer, Navbar},
   mounted : async function mounted() {
     // fetch products
     const response1  = await fetch(this.baseURL + "product/");
@@ -41,47 +31,11 @@ export default {
 html{
   overflow-y: scroll;
 }
-
 #app {
   font-family: 'Lato', 'Arial', sans-serif;
   font-weight: 500;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-.nav{
-  background-image: url("./assets/nav-back.png");
-  justify-content: center;
-  font-size: 25px;
-}
-.nav img {
-  margin : 10px;
-  border-radius: 20px;
-}
-.logo{
-  height : 90px;
-  width : auto;
-  float : left;
-  margin-top : 20px;
-}
-.main-nav {
-  float : right;
-  list-style: none;
-  margin : 35px 0 0 640px;
-}
-.main-nav li {
-  display : inline-block;
-  margin-left:40px;
-  font-size : 19px;
-}
-.main-nav a,
-.main-nav a:hover{
-  font-weight:700;
-  color : rgb(85, 49, 25);
-  text-decoration : none;
-}
-.main-nav a:hover{
-  padding-bottom : 5px;
-  border-bottom:2px solid #4d380c;
 }
 </style>
