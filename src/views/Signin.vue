f6db7ea7f76d447db220131033dcb06edb655dc0
chathurabuddi <chathu.buddi@gmail.com>
2021-01-05 10:08:13 +0530

    loading spinner added while signing in

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -10,7 +10,12 @@
         <label>Password</label>
         <input type="password" class="form-control" v-model="password" required>
       </div>
-      <button type="submit" class="btn btn-primary">Signin</button>
+      <button type="submit" class="btn btn-primary">
+        Signin
+        <div v-if="loading" class="spinner-border spinner-border-sm" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </button>
   </form>
   <br/>
   Don't have an account?&nbsp;&nbsp;<router-link :to="{name: 'Signup'}">Signup Here</router-link>
@@ -24,12 +29,14 @@ export default {
   data() {
       return {
           email: null,
-          password: null
+          password: null,
+          loading: null
       }
   },
   methods : {
     signin : async function(e) {
         e.preventDefault();
+        this.loading = true;
         const user = {
             email: this.email,
             password: this.password
@@ -51,8 +58,14 @@ export default {
         })
         .catch((err) => {
           alert(err);
+        })
+        .finally(() => {
+          this.loading = false;
         });
     }
+  },
+  mounted() {
+    this.loading = false;
   }
 }
 </script>
@@ -62,5 +75,8 @@ export default {
     text-align : center;
     font-size : 60px;
     margin : 30px 0;
+}
+.spinner-border {
+  margin-left: 10px;
 }
 </style>
