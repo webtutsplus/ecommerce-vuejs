725c48b6e802737875b4143c6535836f7a89f7f6
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2021-01-18 11:12:01 +0530

    Updating repo before npm install

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 #cd to ecommere-ui
 #sh run.sh
 apt install npm
-npm install
 git checkout master
 git pull origin master
+npm install
 sudo kill -9 `sudo lsof -t -i:8000`
 nohup npm run serve -- --port 8000 &
