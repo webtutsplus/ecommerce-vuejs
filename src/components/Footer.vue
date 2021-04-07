f77c34224b7fa55e91dbfbd304c9c31d55ddc27b
Shivam Agarwal <shivam.agrawal.2000@gmail.com>
2021-04-07 15:22:51 +0530

    Fixed Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,51 @@
 <template>
   <footer>
-    <div class="container">
+    <div class="container pt-5">
       <div class="row">
-        <div class="col-6">
-          <ul class="footer-nav">
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Android App</a></li>
-            <li><a href="#">iOS App</a></li>
+        <div class="col-3">
+          <ul style="list-style-type: none">
+            <li class="text-light font-weight-bold pb-2">Get to Know Us</li>
+            <li><a class="footer-link font-weight-light" href="#">About Us</a></li>
+            <li><a class="footer-link font-weight-light" href="#">Android App</a><br></li>
+            <li><a class="footer-link font-weight-light" href="#">iOS App</a><br></li>
           </ul>
         </div>
-        <div class="col-6">
-          <ul class="social-links">
-            <li><a href="#"><ion-icon name="logo-facebook" v-pre></ion-icon></a></li>
-            <li><a href="#"><ion-icon name="logo-twitter" v-pre></ion-icon></a></li>
-            <li><a href="#"><ion-icon name="logo-instagram" v-pre></ion-icon></a></li>
-            <li><a href="#"><ion-icon name="logo-google" v-pre></ion-icon></a></li>
+
+        <div class="col-3">
+          <ul style="list-style-type: none">
+            <li class="text-light font-weight-bold">Connect With Us</li>
+            <li><a class="footer-link font-weight-light" href="#">Facebook</a></li>
+            <li><a class="footer-link font-weight-light" href="#">Twitter</a><br></li>
+            <li><a class="footer-link font-weight-light" href="#">Instagram</a><br></li>
+          </ul>
+        </div>
+
+        <div class="col-3">
+          <ul style="list-style-type: none">
+            <li class="text-light font-weight-bold">Make Money With Us</li>
+            <li><a class="footer-link font-weight-light" href="#">Sell with Us</a></li>
+            <li><a class="footer-link font-weight-light" href="#">Become an Affiliate</a><br></li>
+            <li><a class="footer-link font-weight-light" href="#">Advertise Your Products</a><br></li>
+          </ul>
+        </div>
+
+        <div class="col-3">
+          <ul style="list-style-type: none">
+            <li class="text-light font-weight-bold">Let Us Help You</li>
+            <li><a class="footer-link font-weight-light" href="#">Return Centre</a></li>
+            <li><a class="footer-link font-weight-light" href="#">100% Purchase Protection</a><br></li>
+            <li><a class="footer-link font-weight-light" href="#">Help</a><br></li>
+            <li><a class="footer-link font-weight-light" href="#">App Download</a><br></li>
           </ul>
         </div>
       </div>
-      <div>
-        <p>Webtutsplus Market</p>
+
+      <div class="row">
+        <div class="col-12">
+          <div class="text-center py-5">
+            <img id="logo" src="../assets/logo.png" />
+          </div>
+        </div>
       </div>
     </div>
   </footer>
@@ -34,90 +60,30 @@ export default {
 <style>
 footer {
   margin-top : 100px;
-  background-color:rgb(51, 51, 51);
+  background-color:#232F3E;
   font-size:16px;
 }
 
-footer p{
-  margin-bottom : 0;
+li {
+  padding-left: 0;
+  padding-bottom: 10px;
 }
 
-.footer-nav {
-  list-style: none;
-  padding:50px 0 80px 0; 
-  float:left;
+a {
+  text-decoration: none;
 }
 
-.social-links {
-  list-style : none;
-  padding:50px 0 80px 0px;
-  float:right;
+.footer-link {
+  color: #DDD;
 }
 
-.footer-nav li,
-.social-links li {
-  display:inline-block;
-  margin-right:20px;
+#logo {
+  width: 100px;
 }
 
-.footer-nav li:last-child,
-.social-links li:last-child {
-  margin-right:0;
+footer {
+  font-family: 'Roboto', sans-serif;
 }
 
-.footer-nav li a:link,
-.footer-nav li a:visited {
-    text-decoration:none;
-    border:0;
-    color:#888;
-    transition : color 0.2s;
-}
 
-.footer-nav li a:hover,
-.footer-nav li a:active{
-  color:#ddd;
-}
-
-.social-links li a:link,
-.social-links li a:visited {
-  text-decoration:none;
-  border:0;
-  color:#888;
-  transition : color 0.2s;
-  font-size : 24px;
-}
-
-ion-icon[name=logo-facebook],
-ion-icon[name=logo-twitter],
-ion-icon[name=logo-google],
-ion-icon[name=logo-instagram] {
-  transition : color 0.2s;
-}
-
-ion-icon[name=logo-facebook]:hover,
-ion-icon[name=logo-facebook]:active {
-  color :#1877f2;
-}
-
-ion-icon[name=logo-twitter]:hover,
-ion-icon[name=logo-twitter]:active {
-  color : #1da1f2;
-}
-
-ion-icon[name=logo-google]:hover,
-ion-icon[name=logo-google]:active {
-  color : #ea4335;
-}
-
-ion-icon[name=logo-instagram]:hover,
-ion-icon[name=logo-instagram]:active {
-  color : #c32aa3;
-}
-
-footer p {
-  color:#888;
-  margin-top:30px;
-  padding-bottom:5px;
-  text-align:center;
-}
 </style>
