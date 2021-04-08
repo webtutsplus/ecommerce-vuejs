5822b50f57e83f5633753956e22a397ae1eac68c
Shivam Agarwal <shivam.agrawal.2000@gmail.com>
2021-04-08 18:01:01 +0530

    Renovated Product Details Page

--- src/views/Product/ShowDetails.vue
+++ src/views/Product/ShowDetails.vue
@@ -1,66 +1,63 @@
 <template>
-
     <div class="container">
-
-      <div class="row">
-
-        <div class="col-sm">
+      <div class="row pt-5">
+        <div class="col-md-1">
+        </div>
+        <div class="col-md-4 col-12">
           <img :src="product.imageURL" :alt="product.name" class="img-fluid">
         </div>
+        <div class="col-md-6 col-12 pt-3 pt-md-0">
+          <h4>{{product.name}}</h4>
+          <h6 class="category font-italic">{{category.categoryName}}</h6>
+          <p><span class="font-weight-bold">Description: -</span> <br>{{product.description}}</p>
 
-        <div class="col-sm">
+          <div class="d-flex flex-row justify-content-between">
+            <div class="input-group col-md-3 col-4 p-0">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">Quantity</span>
+              </div>
+              <input class="form-control" type="number" v-bind:value="quantity"/>
+            </div>
 
-          <h1>{{product.name}}</h1>
-          <h6 class="category">{{category.categoryName}}</h6>
-          <h5>Price: <span>$ {{product.price}}</span></h5>
-          <h5>{{product.description}}</h5>
-          <div>
-            <label>Quantity</label>
-            <input type="number" v-model="quantity"/>
+            <div class="input-group col-md-3 col-4 p-0">
+              <button type="button" id="add-to-cart-button" class="btn" @click="addToCart(this.id)">
+                Add to Cart
+                <ion-icon name="cart-outline" v-pre></ion-icon>
+              </button>
+            </div>
           </div>
 
-          <div class="features">
+<!--          <div class="invisible">-->
+<!--          <button type="button" class="btn btn-success btn-lg .d-none">-->
+<!--            Buy Now-->
+<!--            <ion-icon name="card-outline" v-pre></ion-icon>-->
+<!--          </button>-->
+<!--          </div>-->
+
+
+
+          <div class="features pt-3">
             <h5><strong>Features</strong></h5>
             <ul>
-              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li> 
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
               <li>Officia quas, officiis eius magni error magnam voluptatem</li>
               <li>nesciunt quod! Earum voluptatibus quaerat dolorem doloribus</li>
               <li>molestias ipsum ab, ipsa consectetur laboriosam soluta et</li>
-              <li>ut doloremque dolore corrupti, architecto iusto beatae.</li> 
+              <li>ut doloremque dolore corrupti, architecto iusto beatae.</li>
             </ul>
           </div>
 
-        </div>
-
-        <div class="col-sm">
-
-            <div class="invisible">
-            <button type="button" class="btn btn-success btn-lg .d-none">
-              Buy Now 
-              <ion-icon name="card-outline" v-pre></ion-icon>
+            <button id="wishlist-button" class="btn mr-3 p-1 py-0" :class="{product_added_wishlist: isAddedToWishlist}" @click="addToWishList(this.id)">
+              {{wishlistString}}
             </button>
-            </div>
+            <button id="show-cart-button" type="button" class="btn" @click="listCartItems()">
+              Show Cart
+              <ion-icon name="cart-outline" v-pre></ion-icon>
+            </button>
 
-            <div class="row mt-2">
-              <button type="button" class="btn btn-primary btn-lg" @click="addToCart(this.id)">
-                Add to Cart   
-                <ion-icon name="cart-outline" v-pre></ion-icon>
-              </button>
-            </div>
+        </div>
+        <div class="col-md-1">
 
-            <div class="row mt-2">
-              <button class="btn btn-primary btn-lg" :class="{product_added_wishlist: isAddedToWishlist}" @click="addToWishList(this.id)">
-                {{wishlistString}}
-              </button>
-            </div>
-
-            <div class="row mt-2">
-              <button type="button" class="btn btn-primary btn-lg" @click="listCartItems()">
-                Show Cart
-                <ion-icon name="cart-outline" v-pre></ion-icon>
-              </button>
-            </div>
-          
         </div>
 
       </div>
@@ -79,7 +76,7 @@ export default {
       token: null,
       isAddedToWishlist: false,
       wishlistString:"Add to wishlist",
-      quantity: 1,
+      quantity: 1
     }
   },
   props : ["baseURL","products", "categories"],
@@ -132,3 +129,35 @@ export default {
   }
 }
 </script>
+
+<style>
+.category {
+  font-weight: 400;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
+#add-to-cart-button {
+  background-color: #febd69;
+}
+
+#wishlist-button {
+  background-color: #b9b9b9;
+  border-radius: 0;
+}
+
+#show-cart-button {
+  background-color: #131921;
+  color: white;
+}
+</style>
