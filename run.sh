79f0406652806cba05bff970cd1af55b463317e9
nmadhab <nilmadhab.mondal@booking.com>
2021-10-09 14:45:40 +0200

    width bug fix

--- run.sh
+++
@@ -1,9 +0,0 @@
-#check read.me to how to log in
-#cd to ecommere-ui
-#sh run.sh
-apt install npm
-git checkout master
-git pull origin master
-npm install
-sudo kill -9 `sudo lsof -t -i:8000`
-nohup npm run serve -- --port 8000 &
